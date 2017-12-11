cb9bc70694892d2704596eb011e6c7fb2c8098e7
kw7oe <choongkwern@hotmail.com>
2017-12-12 07:44:52 +0800

    Integrate AR with Member

--- lib/trello_todo.rb
+++ lib/trello_todo.rb
@@ -1,16 +1,34 @@
 require 'trello_todo/version'
 require 'trello_todo/cli'
 require 'trello_todo/trello/member'
+require 'yaml'
+require 'active_record'
+
+db_config = YAML.load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(db_config['development'])
+
+unless File.exist?(db_config['development']['database'])
+  ActiveRecord::Base.connection
+  ActiveRecord::Migrator.migrate("db/migrate/")
+  ActiveRecord::Base.establish_connection(db_config['development'])
+  require 'active_record/schema_dumper'
+  filename = "db/schema.rb"
+  File.open(filename, "w:utf-8") do |file|
+    ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, file)
+  end
+end
 
 module TrelloTodo
   def self.run(args)
     abort('Username is required') if args.empty?
-    m = Member.new(username: args[0])
-    CLI.show_boards_of(member: m)
-    print 'Please select a board: '
-    i = $stdin.gets.chomp.to_i
-    CLI::Util.clear_line(m.boards.size + 3)
-    b = m.boards[i - 1]
-    CLI.show_lists_of(board: b)
+
+    m = Member.create!(name: args[0])
+    p m
+    #    CLI.show_boards_of(member: m)
+    #    print 'Please select a board: '
+    #    i = $stdin.gets.chomp.to_i
+    #    CLI::Util.clear_line(m.boards.size + 3)
+    #    b = m.boards[i - 1]
+    #    CLI.show_lists_of(board: b)
   end
 end
--- lib/trello_todo/trello/member.rb
+++ lib/trello_todo/trello/member.rb
@@ -1,28 +1,30 @@
 require 'trello_todo/trello/board'
+require 'active_record'
 
 module TrelloTodo
-  class Member
-    attr_reader :username
-
-    def initialize(username:)
-      @username = username
-      @boards = []
-    end
-
-    # Get all the boards of a given user
-    def boards
-      return @boards unless @boards.empty?
-
-      endpoint = "/1/members/#{username}/boards?"
-      response = Client.get(endpoint)
-
-      @boards = response.map do |board|
-        Board.new(id: board['id'], name: board['name'])
-      end
-    end
-
-    def to_s
-      username
-    end
+  class Member < ActiveRecord::Base
+#    attr_reader :username
+#
+#    def initialize(username:)
+#      @username = username
+#      @member = Member.create!(name: username)
+#      @boards = []
+#    end
+#
+#    # Get all the boards of a given user
+#    def boards
+#      return @boards unless @boards.empty?
+#
+#      endpoint = "/1/members/#{username}/boards?"
+#      response = Client.get(endpoint)
+#
+#      @boards = response.map do |board|
+#        Board.new(id: board['id'], name: board['name'])
+#      end
+#    end
+#
+#    def to_s
+#      username
+#    end
   end
 end
